aeeffc172c958a3a2be05f1c27b4dc6e21b1a12b
JorgeArcia <84509951+JorgeArcia@users.noreply.github.com>
2021-07-29 09:56:52 -0300

    Carga DungeonsAndDragons
    Se carga carpeta local del juego DungeonsAndDragons

---
+++ DungeonsAndDragon/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsAndDragon
+{
+    class Personaje
+    {
+        public float Fuerza, Agilidad, Aguante, FuerzaDelArma, PesoDelArma, Escudo, Vida;
+
+        public float AtackBase()
+        {
+            float DanioAtaque = ((Fuerza + Agilidad) - (PesoDelArma % Aguante));
+            if (Fuerza<PesoDelArma)
+            {
+                Console.WriteLine("Penalidad por exceso de peso.");
+                FuerzaDelArma = FuerzaDelArma * 0.98f;
+            }
+            if (PesoDelArma % FuerzaDelArma == 0)
+            {
+                Console.WriteLine("Obtuviste Bonificacion por arma equilibrada");
+                DanioAtaque = DanioAtaque * 1.03f;
+            }
+            return DanioAtaque;
+        }
+
+        public float Defence(bool EstatusEscudo)
+        {
+            float defensa=0;
+            if (EstatusEscudo==true)
+            {
+                defensa = (Aguante * Agilidad);
+            }
+            else
+            {
+                defensa = ((Aguante * Agilidad) + (Escudo + (Escudo * 1.50f)));
+            }
+            return defensa;
+        }
+
+
+
+    }
+}
